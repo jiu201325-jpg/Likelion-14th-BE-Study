fdbdb906310ca429ff1ca44e19556d2af6f7e3d1
githeobu <heominji1012@gmail.com>
2026-04-04 18:01:09 +0900

    [Week04] 허민지 과제 제출

---
+++ HeoMinJi/week04/src/main/java/com/example/hellospring/dto/StudentJoinRequest.java
@@ -0,0 +1,28 @@
+package com.example.hellospring.dto;
+
+import com.example.hellospring.entity.Student;
+
+public class StudentJoinRequest {
+
+    // 학생 등록 요청에 필요한 데이터
+    // 사용자가 보내는 JSON키와 대응됨.
+    private String name;
+    private String email;
+
+    public StudentJoinRequest(){} //기본 생성자
+    public StudentJoinRequest(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+    // DTO를 Entity로 바꾸는 메서드
+    public Student toEntity(){
+        return new Student(name,email);
+    }
+    // 게터 메서드
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
---
+++ HeoMinJi/week04/src/main/java/com/example/hellospring/entity/Student.java
@@ -0,0 +1,26 @@
+package com.example.hellospring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter // 롬보 기능(getter 만들어줌)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 기본 생성자 만들어줌.
+public class Student {
+    @Id // 이 필드가 기본키라는 의미
+    @GeneratedValue // id 값 안 넣어도 DB가 자동으로 만들어줄게.
+    private long id;
+
+    private String name;
+    private String email;
+
+    public Student(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
